6064fa697ff32c1f78a181b3e3967d29bfdcbce8
SongMadison <songwang@stat.wisc.edu>
2016-09-26 02:21:17 -0500

    update

--- 5.R
+++ 5.R
@@ -1,27 +1,49 @@
 #download Trump 's follower list
+rm(list =ls())
+load('credential.RData')
+
+
 library(dplyr)
 library('RSQLite')
-library(data.table)
+if (!require('data.table')){
+  install.packages("data.table")
+  library(data.table)
+}else{
+  library(data.table)
+}
+library(twitteR)
 
 
+setup_twitter_oauth(consumer_key, consumer_secret,access_token, access_secret)
+
+
+
+
+p1 <- proc.time()
 ur <- getUser(user = 'realDonaldTrump')  # return a user object
 # lookupUsers(users = list of users ) # return a list of user objects
 #
-ur.followerIDs <- ur$getFollowerIDs(n = 10000, retryOnRateLimit = 15) ## a list of IDs
-ur.followers <- ur$getFollowers(n = 10000, retryOnRateLimit = 15)
-system.time ( ur.followers.info <-  do.call('rbind',lapply(ur.followers, as.data.frame)))
-system.time(a <- rbindlist(lapply(ur.followers,as.data.frame)))
-
-favorited_twittes <- list()
-p1 <- proc.time()
-for (user_obj in ur.followers[1:100]){
-    favorited_twittes[[user_obj$id]] <- favorites(user = user_obj, n = 200, retryOnRateLimit = 15)
-}
+#ur.followerIDs <- ur$getFollowerIDs( retryOnRateLimit = 15) ## n =100, a list of IDs
+ur.followers <- ur$getFollowers(n = 1440000, retryOnRateLimit = 15)
 proc.time()-p1
-#
-tweets_db <- src_sqlite(path = "tweets_db.sqlite3", create = T)
-register_sqlite_backend('tweets_db.sqlite3')
-search_twitter_and_store("swang282",table_name = 'myself')
+# 20k -- 20 mins
+save.image(file = "followers_whole.RData" )
+
+#system.time ( ur.followers.info <-  do.call('rbind',lapply(ur.followers, as.data.frame)))
+system.time(ur.followers.info <- rbindlist(lapply(ur.followers,as.data.frame)))
 
 
-favorites(user = 'swang282')
+proc.time() - p1
+# favorited_twittes <- list()
+# p1 <- proc.time()
+# for (user_obj in ur.followers[1:100]){
+#     favorited_twittes[[user_obj$id]] <- favorites(user = user_obj, n = 200, retryOnRateLimit = 15)
+# }
+# proc.time()-p1
+# #
+# tweets_db <- src_sqlite(path = "tweets_db.sqlite3", create = T)
+# register_sqlite_backend('tweets_db.sqlite3')
+# search_twitter_and_store("swang282",table_name = 'myself')
+# 
+# 
+# favorites(user = 'swang282')
